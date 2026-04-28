d118f69cea5e987fab041384ee09faa34332356c
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-04-29 00:38:13 +0530

    Add employees table with sample data
    Create employees table and insert initial data.

---
+++ SQL/01_Basics/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE employees (
+    emp_id INT,
+    name VARCHAR(50),
+    dept VARCHAR(50),
+    salary INT
+);
+
+INSERT INTO employees VALUES
+(1,'Ravi','IT',50000),
+(2,'Priya','HR',45000),
+(3,'Arjun','Finance',60000),
+(4,'Sneha','IT',55000),
+(5,'Kiran','Sales',40000);
