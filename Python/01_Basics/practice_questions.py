3f3cdbbb08e7ac8de0ff07c708a34ee89b16e62c
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-04-29 18:57:20 +0530

    Create practice_questions.py

---
+++ Python/01_Basics/practice_questions.py
@@ -0,0 +1,12 @@
+Python Basics Practice Questions
+
+ 1. Print your name
+ 2. Print your age
+ 3. Store your name in a variable and print it
+ 4. Store two numbers and print their sum
+ 5. Take user input and print it
+ 6. Convert string "25" into integer
+ 7. Print data type of a variable
+ 8. Swap two variables
+ 9. Find square of a number
+ 10. Print multiple values in one line
