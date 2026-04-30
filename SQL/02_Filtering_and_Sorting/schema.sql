1867118f63e72b3ba09b6b5a767c7323e3e72f97
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-04-30 18:23:26 +0530

    Create schema.sql

---
+++ SQL/02_Filtering_and_Sorting/schema.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE employees (
+    emp_id INT,
+    emp_name VARCHAR(50),
+    dept VARCHAR(50),
+    salary INT,
+    city VARCHAR(50)
+);
+
+INSERT INTO employees VALUES
+(1,'Ravi','IT',50000,'Hyderabad'),
+(2,'Priya','HR',45000,'Chennai'),
+(3,'Arjun','Finance',60000,'Bangalore'),
+(4,'Sneha','IT',55000,'Hyderabad'),
+(5,'Kiran','Sales',40000,'Mumbai'),
+(6,'Meena','HR',70000,'Delhi');
