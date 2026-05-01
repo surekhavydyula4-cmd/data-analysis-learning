4261bfe4c728ff0b8605236726749370d1b56d8c
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-05-01 20:33:35 +0530

    Add practice questions for conditions in Python

---
+++ Python/02_Conditions/practice_questions.py
@@ -0,0 +1,14 @@
+python Chapter 2 - Conditions
+Practice Questions
+
+# 1. Check if a number is positive or negative
+# 2. Check if a number is even or odd
+# 3. Find greater of two numbers
+# 4. Check if a person is eligible to vote (18+)
+# 5. Check if a year is leap year or not
+# 6. Find greatest of three numbers
+# 7. Check if a character is vowel or consonant
+# 8. Check if number is divisible by 5 and 11
+# 9. Assign grade based on marks:
+#    90+ A, 75+ B, 50+ C, else Fail
+# 10. Check login password using if else
