8ec196309d8043c00721ae2ffcb05a5884fe5e3e
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-05-01 21:11:42 +0530

    Update practice_questions.sql

--- SQL/01_Basics/practice_questions.sql
+++ SQL/01_Basics/practice_questions.sql
@@ -1,10 +1,10 @@
-1.Show all employee records.
-2.Show only employee names.
-3.Show unique department names.
-4.Show employee names with salary.
-5.Rename salary column as employee_salary.
-6.Show first 3 rows.
-7.Show employees from IT department.
-8.Show employees with salary greater than 50000.
-9.Show employee names in ascending order.
-10.Show all data sorted by salary descending.
+-- 1.Show all employee records.
+-- 2.Show only employee names.
+-- 3.Show unique department names.
+-- 4.Show employee names with salary.
+-- 5.Rename salary column as employee_salary.
+-- 6.Show first 3 rows.
+-- 7.Show employees from IT department.
+-- 8.Show employees with salary greater than 50000.
+-- 9.Show employee names in ascending order.
+-- 10.Show all data sorted by salary descending.
