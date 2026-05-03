6021983bd10d685e7092da78f106cb1a24918bca
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-05-03 21:38:25 +0530

    Add practice questions for loops in Python

---
+++ Python/03_Loops/practice_questions.py
@@ -0,0 +1,18 @@
+# Python Chapter 3 - Loops
+# Practice Questions
+
+# 1. Print numbers from 1 to 10
+# 2. Print numbers from 10 to 1
+# 3. Print even numbers from 1 to 20
+# 4. Print sum of numbers from 1 to n
+# 5. Print multiplication table of a number
+# 6. Count digits in a number
+# 7. Reverse a number
+# 8. Check if a number is palindrome
+# 9. Print factorial of a number
+# 10. Print star pattern:
+# *
+# **
+# ***
+# ****
+# *****
