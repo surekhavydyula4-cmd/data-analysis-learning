4f2d88bb906787f1fb446b31242e284020ae2e15
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-05-06 22:37:17 +0530

    Create practice_questions.sql for SQL subqueries
    Added practice questions for SQL subqueries.

---
+++ SQL/05_Subqueries/practice_questions.sql
@@ -0,0 +1,21 @@
+-- SQL Chapter 5 - Subqueries 
+
+-- 1. Find employees who earn more than the average salary
+
+-- 2. Find employees who earn the highest salary
+
+-- 3. Find employees who earn less than the average salary
+
+-- 4. Find employees who work in the same department as 'Ravi'
+
+-- 5. Find employees whose salary is greater than all employees in HR department
+
+-- 6. Find departments where average salary is greater than 50000
+
+-- 7. Find employees who are not in the IT department
+
+-- 8. Find employees whose salary is equal to the minimum salary
+
+-- 9. Find employees whose salary is above the average salary of their own department
+
+-- 10. Find the second highest salary using subquery
