0791d550418cf8874586c3c986d00def90d050d6
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-05-06 22:57:36 +0530

    Update practice_questions.sql

--- SQL/05_Subqueries/practice_questions.sql
+++ SQL/05_Subqueries/practice_questions.sql
@@ -1,21 +1,21 @@
 -- SQL Chapter 5 - Subqueries 
 
--- 1. Find employees who earn more than the average salary
+-- 1. Find employees who earn more than the average salary of the company
 
--- 2. Find employees who earn the highest salary
+-- 2. Find employees whose salary is greater than the salary of employee 'Ravi'
 
--- 3. Find employees who earn less than the average salary
+-- 3. Find employees who work in departments where the average salary is greater than 50000
 
--- 4. Find employees who work in the same department as 'Ravi'
+-- 4. Find employees whose salary is greater than ALL employees in HR department
 
--- 5. Find employees whose salary is greater than all employees in HR department
+-- 5. Find employees whose salary is greater than ANY employee in Finance department
 
--- 6. Find departments where average salary is greater than 50000
+-- 6. Find employees who do NOT work in the same department as 'Ravi'
 
--- 7. Find employees who are not in the IT department
+-- 7. Find employees whose salary is above the average salary of their own department
 
--- 8. Find employees whose salary is equal to the minimum salary
+-- 8. Find employees who have the same salary as at least one other employee
 
--- 9. Find employees whose salary is above the average salary of their own department
+-- 9. Find employees whose salary is the second highest in the company
 
--- 10. Find the second highest salary using subquery
+-- 10. Find employees who earn more than the minimum salary in their department
