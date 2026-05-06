6321ba3bfa704a6bbcb63f7dfed8ed8666093764
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-05-06 22:35:53 +0530

    Create schema.sql

---
+++ SQL/05_Subqueries/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE employees (
+    emp_id INT,
+    emp_name VARCHAR(50),
+    dept VARCHAR(50),
+    salary INT
+);
+
+INSERT INTO employees VALUES
+(1,'Ravi','IT',50000),
+(2,'Priya','HR',45000),
+(3,'Arjun','Finance',60000),
+(4,'Sneha','IT',55000),
+(5,'Kiran','Sales',40000),
+(6,'Meena','HR',48000),
+(7,'Rahul','IT',70000);
