53f631dcc923f7154df83b362a6352d151d11107
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-05-08 18:36:45 +0530

    Create practice_questions.sql

---
+++ SQL/06_Window_Functions/practice_questions.sql
@@ -0,0 +1,21 @@
+-- SQL Chapter 6 - Window Functions Practice
+
+-- 1. Assign row numbers to all employees based on salary
+
+-- 2. Rank employees based on salary
+
+-- 3. Find dense rank of employees by salary
+
+-- 4. Assign row numbers within each department
+
+-- 5. Find highest paid employee in each department
+
+-- 6. Find second highest salary in the company using DENSE_RANK()
+
+-- 7. Find previous employee salary using LAG()
+
+-- 8. Find next employee salary using LEAD()
+
+-- 9. Calculate running total of salaries
+
+-- 10. Find employees whose salary is greater than previous employee salary
