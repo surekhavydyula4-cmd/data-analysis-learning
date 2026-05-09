7ca62946cfc2544ea401564a3bc8dfae3143b503
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-05-09 20:36:20 +0530

    Create practice_questions.py for tuples and sets
    Added practice questions for tuples and sets.

---
+++ Python/06_Tuples_Sets/practice_questions.py
@@ -0,0 +1,41 @@
+# Python Chapter 6 - Tuples & Sets Practice Questions
+
+# 1. Create a tuple and print all elements
+
+# 2. Print first and last element of a tuple
+
+# 3. Find length of a tuple
+
+# 4. Count occurrence of an element in tuple
+
+# 5. Find index of an element in tuple
+
+# 6. Perform tuple unpacking
+
+# 7. Create a set and print all elements
+
+# 8. Add elements to a set
+
+# 9. Remove elements using remove()
+
+# 10. Remove elements using discard()
+
+# 11. Find union of two sets
+
+# 12. Find intersection of two sets
+
+# 13. Find difference between two sets
+
+# 14. Remove duplicate elements from a list using set
+
+# 15. Check whether an element exists in a set
+
+# 16. Find maximum element in a set
+
+# 17. Find minimum element in a set
+
+# 18. Find sum of elements in a set
+
+# 19. Convert tuple to list
+
+# 20. Convert list to tuple
