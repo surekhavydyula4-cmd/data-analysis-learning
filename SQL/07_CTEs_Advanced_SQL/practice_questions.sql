72521b15d2264d2ccba195f3706bc2b2eedd3ff8
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-05-12 20:30:49 +0530

    Create practice_questions.sql

---
+++ SQL/07_CTEs_Advanced_SQL/practice_questions.sql
@@ -0,0 +1,25 @@
+-- SQL Chapter 7 - CTEs & Advanced SQL Practice
+
+
+-- 1. Create a CTE to show employees with salary > 50000
+
+-- 2. Find highest salary using CTE
+
+-- 3. Find average salary department-wise using CTE
+
+-- 4. Use CASE WHEN to categorize salaries
+--    >60000 = High
+--    40000-60000 = Medium
+--    <40000 = Low
+
+-- 5. Replace NULL values using COALESCE()
+
+-- 6. Avoid division by zero using NULLIF()
+
+-- 7. Find employees whose department exists in another query using EXISTS
+
+-- 8. Find employees whose department does not exist using NOT EXISTS
+
+-- 9. Create multiple CTEs and join them
+
+-- 10. Find second highest salary using CTE
