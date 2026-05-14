0d5d64f51cb127751bd41bd227bedc0ea72e8de3
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-05-14 20:58:31 +0530

    Add practice questions for SQL indexes

---
+++ SQL/08_Indexes/practice_questions.sql
@@ -0,0 +1,22 @@
+-- SQL Chapter 8 - Indexes 
+-- Practice Questions
+
+-- 1. Create an index on employee name column
+
+-- 2. Create an index on salary column
+
+-- 3. Create a composite index on department and salary
+
+-- 4. Create a unique index on email column
+
+-- 5. Display employee data using indexed column
+
+-- 6. Find employees using salary condition
+
+-- 7. Drop an existing index
+
+-- 8. Create index on joining_date column
+
+-- 9. Create composite index on city and department
+
+-- 10. Drop composite index
