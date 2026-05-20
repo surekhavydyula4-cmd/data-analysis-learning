4aabb1c4addb06c51bdf983adea9669fcf087e43
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-05-20 21:47:55 +0530

    Create practice_questions.sql for stored procedures
    Added practice questions for SQL stored procedures.

---
+++ SQL/10_Stored_Procedures/practice_questions.sql
@@ -0,0 +1,22 @@
+-- SQL Chapter 10 - Stored Procedures
+-- Practice Questions
+
+-- 1. Create a procedure to display all employees
+
+-- 2. Call the procedure
+
+-- 3. Create a procedure to display employees by department
+
+-- 4. Create a procedure with salary parameter
+
+-- 5. Create a procedure using IN parameter
+
+-- 6. Create a procedure using OUT parameter
+
+-- 7. Create a procedure using INOUT parameter
+
+-- 8. Create a procedure to count employees
+
+-- 9. Drop a procedure
+
+-- 10. Create a procedure to display employees with salary > 60000
