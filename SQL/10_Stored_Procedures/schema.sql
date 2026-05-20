64a3d7b07d6d56965ad409c9ce0db68b9547925b
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-05-20 21:46:55 +0530

    Create schema.sql

---
+++ SQL/10_Stored_Procedures/schema.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE employees(
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    dept VARCHAR(30),
+    salary INT
+);
+
+INSERT INTO employees VALUES
+(101,'Sai','HR',55000),
+(102,'Rahul','IT',70000),
+(103,'Sneha','Finance',65000),
+(104,'Akhil','IT',80000),
+(105,'Keerthi','HR',60000);
