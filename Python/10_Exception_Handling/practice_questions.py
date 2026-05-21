375c9d1369232ae5ca4a1065062f7841226933a8
Surekha Vydyula <surekhavydyula4@gmail.com>
2026-05-21 22:04:49 +0530

    Create practice_questions.py

---
+++ Python/10_Exception_Handling/practice_questions.py
@@ -0,0 +1,22 @@
+# Python Chapter 10 - Exception Handling 
+# Practice Questions
+
+# 1. Handle division by zero error
+
+# 2. Handle invalid number input
+
+# 3. Use try and except block
+
+# 4. Use try, except, else
+
+# 5. Use finally block
+
+# 6. Handle multiple exceptions
+
+# 7. Raise an exception manually
+
+# 8. Create custom exception
+
+# 9. Handle index error in list
+
+# 10. Handle key error in dictionary
